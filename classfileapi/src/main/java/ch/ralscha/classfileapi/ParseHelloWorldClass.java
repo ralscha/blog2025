f415d38af174ebfe71d520898ec9397209e32bf9
Ralph Schaer <ralphschaer@gmail.com>
2026-03-09 13:16:46 +0100

    Revert "reformat"
    This reverts commit ebc1e902ed5a159b11c88e6d0c253b332259f691.

--- classfileapi/src/main/java/ch/ralscha/classfileapi/ParseHelloWorldClass.java
+++ classfileapi/src/main/java/ch/ralscha/classfileapi/ParseHelloWorldClass.java
@@ -10,28 +10,26 @@ import java.nio.file.Path;
 
 public class ParseHelloWorldClass {
 
-  public static void main(String[] args) throws Exception {
-    Path classFile = Path.of("target", "generated-classes", "Hello.class");
-    ClassModel classModel = ClassFile.of().parse(classFile);
+	public static void main(String[] args) throws Exception {
+		Path classFile = Path.of("target", "generated-classes", "Hello.class");
+		ClassModel classModel = ClassFile.of().parse(classFile);
 
-    System.out.println("Class: " + classModel.thisClass().asSymbol().displayName());
-    System.out.println("Methods:");
+		System.out.println("Class: " + classModel.thisClass().asSymbol().displayName());
+		System.out.println("Methods:");
 
-    for (MethodModel method : classModel.methods()) {
-      System.out.println(
-          "- "
-              + method.methodName().stringValue()
-              + " "
-              + method.methodTypeSymbol().displayDescriptor());
+		for (MethodModel method : classModel.methods()) {
+			System.out.println(
+					"- " + method.methodName().stringValue() + " " + method.methodTypeSymbol().displayDescriptor());
 
-      for (MethodElement methodElement : method) {
-        if (methodElement instanceof CodeModel codeModel) {
-          System.out.println("  Code:");
-          for (CodeElement codeElement : codeModel) {
-            System.out.println("  - " + codeElement);
-          }
-        }
-      }
-    }
-  }
+			for (MethodElement methodElement : method) {
+				if (methodElement instanceof CodeModel codeModel) {
+					System.out.println("  Code:");
+					for (CodeElement codeElement : codeModel) {
+						System.out.println("  - " + codeElement);
+					}
+				}
+			}
+		}
+	}
+
 }
