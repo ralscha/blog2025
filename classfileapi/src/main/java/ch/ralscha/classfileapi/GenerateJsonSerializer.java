ebc1e902ed5a159b11c88e6d0c253b332259f691
Ralph Schaer <ralphschaer@gmail.com>
2026-03-09 13:15:34 +0100

    reformat

--- classfileapi/src/main/java/ch/ralscha/classfileapi/GenerateJsonSerializer.java
+++ classfileapi/src/main/java/ch/ralscha/classfileapi/GenerateJsonSerializer.java
@@ -2,13 +2,11 @@ package ch.ralscha.classfileapi;
 
 public class GenerateJsonSerializer {
 
-	public static void main(String[] args) {
-		JSONSerializer<Person> serializer = JSONSerializer.from(Person.class);
-		String json = serializer.serialize(new Person("Duke", "Java"));
-		System.out.println(json);
-	}
+  public static void main(String[] args) {
+    JSONSerializer<Person> serializer = JSONSerializer.from(Person.class);
+    String json = serializer.serialize(new Person("Duke", "Java"));
+    System.out.println(json);
+  }
 
-	record Person(String firstName, String lastName) {
-	}
-
+  record Person(String firstName, String lastName) {}
 }
--- classfileapi/src/main/java/ch/ralscha/classfileapi/TransformHelloWorldClass.java
+++ classfileapi/src/main/java/ch/ralscha/classfileapi/TransformHelloWorldClass.java
@@ -15,63 +15,69 @@ import java.nio.file.Path;
 
 public class TransformHelloWorldClass {
 
-	public static void main(String[] args) throws Exception {
-		Path originalClass = Path.of("target", "generated-classes", "Hello.class");
-		ClassFile classFile = ClassFile.of();
-		ClassModel classModel = classFile.parse(originalClass);
+  public static void main(String[] args) throws Exception {
+    Path originalClass = Path.of("target", "generated-classes", "Hello.class");
+    ClassFile classFile = ClassFile.of();
+    ClassModel classModel = classFile.parse(originalClass);
 
-		CodeTransform addLoggingAspect = addLoggingAspect();
+    CodeTransform addLoggingAspect = addLoggingAspect();
 
-		byte[] transformedBytes = classFile.transformClass(classModel,
-				ClassTransform.transformingMethodBodies(TransformHelloWorldClass::isMainMethod, addLoggingAspect));
+    byte[] transformedBytes =
+        classFile.transformClass(
+            classModel,
+            ClassTransform.transformingMethodBodies(
+                TransformHelloWorldClass::isMainMethod, addLoggingAspect));
 
-		runGeneratedClass(transformedBytes);
-	}
+    runGeneratedClass(transformedBytes);
+  }
 
-	private static CodeTransform addLoggingAspect() {
-		ClassDesc systemClass = ClassDesc.of("java.lang.System");
-		ClassDesc printStreamClass = ClassDesc.of("java.io.PrintStream");
-		MethodTypeDesc printlnType = MethodTypeDesc.of(ConstantDescs.CD_void, ConstantDescs.CD_String);
-		boolean[] entryInjected = { false };
+  private static CodeTransform addLoggingAspect() {
+    ClassDesc systemClass = ClassDesc.of("java.lang.System");
+    ClassDesc printStreamClass = ClassDesc.of("java.io.PrintStream");
+    MethodTypeDesc printlnType = MethodTypeDesc.of(ConstantDescs.CD_void, ConstantDescs.CD_String);
+    boolean[] entryInjected = {false};
 
-		return (builder, element) -> {
-			if (!entryInjected[0]) {
-				emitPrintln(builder, systemClass, printStreamClass, printlnType, "Start of main");
-				entryInjected[0] = true;
-			}
+    return (builder, element) -> {
+      if (!entryInjected[0]) {
+        emitPrintln(builder, systemClass, printStreamClass, printlnType, "Start of main");
+        entryInjected[0] = true;
+      }
 
-			if (element instanceof ReturnInstruction) {
-				emitPrintln(builder, systemClass, printStreamClass, printlnType, "End of main");
-			}
+      if (element instanceof ReturnInstruction) {
+        emitPrintln(builder, systemClass, printStreamClass, printlnType, "End of main");
+      }
 
-			builder.with(element);
-		};
-	}
+      builder.with(element);
+    };
+  }
 
-	private static void emitPrintln(CodeBuilder builder, ClassDesc systemClass, ClassDesc printStreamClass,
-			MethodTypeDesc printlnType, String message) {
-		builder.getstatic(systemClass, "out", printStreamClass)
-			.ldc(message)
-			.invokevirtual(printStreamClass, "println", printlnType);
-	}
+  private static void emitPrintln(
+      CodeBuilder builder,
+      ClassDesc systemClass,
+      ClassDesc printStreamClass,
+      MethodTypeDesc printlnType,
+      String message) {
+    builder
+        .getstatic(systemClass, "out", printStreamClass)
+        .ldc(message)
+        .invokevirtual(printStreamClass, "println", printlnType);
+  }
 
-	private static boolean isMainMethod(MethodModel methodModel) {
-		return "main".equals(methodModel.methodName().stringValue());
-	}
+  private static boolean isMainMethod(MethodModel methodModel) {
+    return "main".equals(methodModel.methodName().stringValue());
+  }
 
-	private static void runGeneratedClass(byte[] bytes) throws ReflectiveOperationException {
-		GeneratedClassLoader classLoader = new GeneratedClassLoader();
-		Class<?> generatedClass = classLoader.define(bytes);
-		Method mainMethod = generatedClass.getMethod("main", String[].class);
-		mainMethod.invoke(null, (Object) new String[0]);
-	}
+  private static void runGeneratedClass(byte[] bytes) throws ReflectiveOperationException {
+    GeneratedClassLoader classLoader = new GeneratedClassLoader();
+    Class<?> generatedClass = classLoader.define(bytes);
+    Method mainMethod = generatedClass.getMethod("main", String[].class);
+    mainMethod.invoke(null, (Object) new String[0]);
+  }
 
-	private static final class GeneratedClassLoader extends ClassLoader {
+  private static final class GeneratedClassLoader extends ClassLoader {
 
-		private Class<?> define(byte[] bytes) {
-			return defineClass(null, bytes, 0, bytes.length);
-		}
-
-	}
-
+    private Class<?> define(byte[] bytes) {
+      return defineClass(null, bytes, 0, bytes.length);
+    }
+  }
 }
